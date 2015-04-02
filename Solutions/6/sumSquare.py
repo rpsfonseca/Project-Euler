e3d59e408dae97078ad19d14c3c31d82ae78822a
Ricardo Fonseca <ric.bicas@gmail.com>
2015-04-03 00:02:27 +0100

    Finished

--- Solutions/6/sumSquare.py
+++ Solutions/6/sumSquare.py
@@ -1,4 +1,11 @@
+def squareIt(num):
+    return num*num
+
 if __name__ == '__main__':
-    array = range(10)
+    array = range(1,101)
     total = sum(array)
-    squareIt(total) - 
+
+    for i in range(len(array)):
+        array[i] = squareIt(array[i]) 
+   
+    print squareIt(total) - sum(array) 
